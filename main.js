5a467cac9135f2cf05979a5fbc2570117f52055b
Anton0711 <anton.shilo.00@gmail.com>
2024-10-14 18:06:25 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+//калькулятор
+
+
+// const a = document.querySelector(".number-1")
+// const b = document.querySelector(".number-2")
+// const plus = document.querySelector(".plus")
+// const minus = document.querySelector(".minus")
+// const umn = document.querySelector(".umn")
+// const del = document.querySelector(".del")
+// const result = document.querySelector(".result")
+// const res = document.querySelector(".res")
+
+
+// plus.addEventListener('click', function () {
+//     res.innerText = Number(a.value) + Number(b.value);
+// })
+// minus.addEventListener('click', function () {
+//     res.innerText = Number(a.value) - Number(b.value);
+// })
+// del.addEventListener('click', function () {
+//     res.innerText = Number(a.value) / Number(b.value);
+// })
+// umn.addEventListener('click', function () {
+//     res.innerText = Number(a.value) * Number(b.value);
+// })
+
+
+// result.addEventListener('click', function () {
+//     all = [(Number(a.value) + Number(b.value)), (Number(a.value) - Number(b.value)), (Number(a.value) * Number(b.value)), (Number(a.value) / Number(b.value))];
+//     res.innerText = all;
+//     if (b === 0) {
+//         del.addEventListener('click', function () {
+//            all[3] = ("Ошибка, деление на ноль невозможно");
+//         })
+//     }
+
+// })
+
+
+
+
+
+
+// 22222222222222222222222222222222222
+// 55555555555555555555555555555555555
+// 44444444444444444444444444444444444
+
+// const b = document.getElementById("inputField");
+// const a = document.getElementById("inputbtn");
+// const d = document.querySelector(".window__btn")
+// const c = document.getElementById('window')
+
+// a.addEventListener('click', function () {
+//     if (b != "") {
+
+//         b.value = ('привет ' + b.value);
+//         b.style.fontWeight = 'bold';
+//         b.style.fontStyle = 'italic';
+
+//     }
+//     else if (b == "") {
+//         c.style.display = 'block';
+//     }
+// })
+// d.addEventListener('click', function () {
+//     c.style.display = 'none';
+// })
+
+
+
+
+
+
+
+
+
+
+// 333333333333333333333333333333333333333333333333333
+
+
+// var b = 0;
+// const a = document.querySelector("html")
+// a.addEventListener('click', function(){
+//     console.log( b += 1);
+// })
+
+
+
